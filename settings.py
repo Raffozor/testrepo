0c05e5db8c137e3051dccc4c0cee03c0945932cd
Raffaello <52360749+Raffozor@users.noreply.github.com>
2022-11-26 17:28:12 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+
+
+YEAR = 2019 #last year of avilable data 
+GEOID = ["184020500"] #list of GEOid municipalities for each you need to aggregate data
+UPDATE = False #Set True to update latest data from INE database using API (it takes a while), useful to do when new year data is available
+                #Set False to use local data (faster but not updated to latest available data)
